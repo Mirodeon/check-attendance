70dd23b2de6013b63cd3ed2c8e4801e5d2a7c4dd
Mirodeon <mehdi.habaru@gmail.com>
2022-06-03 15:57:07 +0200

    Added passing message

--- pointage.js
+++ pointage.js
@@ -1,18 +1,38 @@
+let currentLocation = "becode";
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender) {
+        console.log(sender.tab ?
+            "from a content script:" + sender.tab.url :
+            "from the extension");
+        if (request.chooseLocation === "becode") {
+            currentLocation = "becode";
+        }
+        else if (request.chooseLocation === "home") {
+            currentLocation = "home";
+        };
+    });
+
+
 function pointage() {
+
     var time = new Date();
     var heure = time.getHours();
     var min = time.getMinutes();
-    if ((heure == 08 && min == 50) || (heure == 12 && min == 30) || (heure == 13 && min == 20) || (heure == 17 && min == 00)) {
+
+    if ((heure == 08 && min == 45) || (heure == 12 && min == 30) || (heure == 13 && min == 20) || (heure == 17 && min == 00)) {
         location.reload();
     } // refresh
-    else if (heure == 08 && min == 51) {
+    else if (heure == 08 && min == 46 && currentLocation === "becode") {
         document.querySelector(".btn-outline-primary").click();
     } // becode
-    /*else if (heure == 08 && min == 51) {
+    else if (heure == 08 && min == 51 && currentLocation === "home") {
         document.querySelector(".btn-outline-secondary").click();
-    } // home*/
+    } // home
     else {
         document.querySelector(".btn-success").click();
-    } // pointage2 
+       /* alert(currentLocation) */ //test
+    } // pointage
 }
-window.setInterval("pointage()", 60000);
+
+window.setInterval("pointage()", 20000);
--- src/js/popup.js
+++ src/js/popup.js
@@ -2,20 +2,23 @@ const locationBtn = document.getElementById("location");
 const activityBtn = document.getElementById("activity");
 let countLocation = 0;
 let countActivity = 0;
-var locationState = "becode";
 
 function switchLocation() {
     if (countLocation == 0) {
-        locationBtn.classList.remove("becode")
-        locationBtn.classList.add("home")
+        locationBtn.classList.remove("becode");
+        locationBtn.classList.add("home");
         countLocation++;
-        locationState = "home";
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {chooseLocation: "home"});
+          });
     }
     else if (countLocation == 1) {
-        locationBtn.classList.remove("home")
-        locationBtn.classList.add("becode")
+        locationBtn.classList.remove("home");
+        locationBtn.classList.add("becode");
         countLocation--;
-        locationState = "becode";
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {chooseLocation: "becode"});
+          });
     }
 }
 
@@ -23,13 +26,13 @@ locationBtn.addEventListener("click", switchLocation);
 
 function switchActivity() {
     if (countActivity == 0) {
-        activityBtn.classList.remove("on-state")
-        activityBtn.classList.add("off-state")
+        activityBtn.classList.remove("on-state");
+        activityBtn.classList.add("off-state");
         countActivity++;
     }
     else if (countActivity == 1) {
-        activityBtn.classList.remove("off-state")
-        activityBtn.classList.add("on-state")
+        activityBtn.classList.remove("off-state");
+        activityBtn.classList.add("on-state");
         countActivity--;
     }
 }
